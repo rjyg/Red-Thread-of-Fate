bcc36d637965b3064c62e289fc614916c4cbc899
rebecca <rjyg98@gmail.com>
2023-12-16 19:54:09 +1100

    panel 2 added

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector('header'); // Get the header
-    const panels = [header, ...document.querySelectorAll('#narrative > div')]; // Include header in the array
+    const header = document.querySelector('header');
+    const panels = [header, ...document.querySelectorAll('#narrative > div'), document.querySelector('footer')];
     const character1 = document.getElementById('character1');
     const character2 = document.getElementById('character2');
+    const character3 = document.getElementById('character3');
 
     function getPanelIndexInView() {
         let panelIndex = 0;
@@ -17,83 +18,244 @@ document.addEventListener("DOMContentLoaded", function() {
         return panelIndex;
     }
 
+    function changeCharacterImages(panelIndex) {
+        let imagePath1, imagePath2, imagePath3;
+        switch (panelIndex) {
+            case 0: // Header
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 1: // Panel 1
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_panel1.svg';
+                imagePath3 = 'images/panel 1 - yue lao.svg';
+                break;
+            case 2: // Panel 2
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 3: // Panel 3
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 4: // Panel 4
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 5: // Panel 5
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 6: // Panel 6
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 7: // Panel 7
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 8: // Panel 8
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            case 9: // Panel 9
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+                break;
+            default:
+                imagePath1 = 'images/panel 1 - boy.svg';
+                imagePath2 = 'images/character2_default.svg';
+                imagePath3 = 'images/panel 2 - yue lao.svg';
+        }
+        // Set image sources
+        character1.src = imagePath1;
+        character2.src = imagePath2;
+        character3.src = imagePath3;
+    }
+
     function moveCharacters(panelIndex) {
-        // Adjusted logic for moving characters based on the panel index, including header
+        if (window.innerWidth <= 960) {
+            // Mobile screens
+            positionCharactersForMobile(panelIndex);
+        } else {
+            // Desktop screens
+            positionCharactersForDesktop(panelIndex);
+        }
+        changeCharacterImages(panelIndex);
+    }
+
+    function positionCharactersForMobile(panelIndex) {
+        // Define positions for mobile screens
         switch(panelIndex) {
             case 0: // Header
-                character1.style.left = '10%';
-                character1.style.top = '80vh';
-                character2.style.left = '90%';
-                character2.style.top = '30vh';
+                character1.style.left = '10vw';
+                character1.style.top = '70vh';
+                character2.style.left = '100vw';
+                character2.style.top = '70vh';
                 console.log("0");
                 break;
             case 1: // Panel 1
-                character1.style.left = '10%';
-                character1.style.top = '180vh';
-                character2.style.left = '50%';
-                character2.style.top = '130vh';
+                character1.style.left = '20vw';
+                character1.style.top = '165vh';
+                character2.style.left = '100vw';
+                character2.style.top = '170vh';
+                character3.style.left = '50vw';
+                character3.style.top = '150vh';
                 break;
             case 2: // Panel 2
-                character1.style.left = '50%';
-                character1.style.top = '250vh';
-                character2.style.left = '50%';
-                character2.style.top = '250vh';
+                character1.style.left = '20vw';
+                character1.style.top = '270vh';
+                character2.style.left = '100vw';
+                character2.style.top = '270vh';
+                character3.style.left = '50vw';
+                character3.style.top = '250vh';
                 break;
             case 3: // Panel 3
-                character1.style.left = '10%';
-                character1.style.top = '320vh';
-                character2.style.left = '50%';
-                character2.style.top = '330vh';
+                character1.style.left = '10vw';
+                character1.style.top = '370vh';
+                character2.style.left = '100vw';
+                character2.style.top = '370vh';
                 break;
             case 4: // Panel 4
-                character1.style.left = '10%';
-                character1.style.top = '420vh';
-                character2.style.left = '50%';
-                character2.style.top = '430vh';
+                character1.style.left = '10vw';
+                character1.style.top = '470vh';
+                character2.style.left = '100vw';
+                character2.style.top = '470vh';
                 break;
             case 5: // Panel 5
-                character1.style.left = '10%';
-                character1.style.top = '520vh';
-                character2.style.left = '50%';
-                character2.style.top = '530vh';
+                character1.style.left = '10vw';
+                character1.style.top = '570vh';
+                character2.style.left = '100vw';
+                character2.style.top = '570vh';
                 break;
             case 6: // Panel 6
-                character1.style.left = '10%';
-                character1.style.top = '620vh';
-                character2.style.left = '50%';
-                character2.style.top = '630vh';
+                character1.style.left = '10vw';
+                character1.style.top = '670vh';
+                character2.style.left = '100vw';
+                character2.style.top = '670vh';
                 break;
             case 7: // Panel 7
-                character1.style.left = '10%';
-                character1.style.top = '720vh';
-                character2.style.left = '50%';
-                character2.style.top = '730vh';
+                character1.style.left = '10vw';
+                character1.style.top = '770vh';
+                character2.style.left = '100vw';
+                character2.style.top = '770vh';
                 break;
             case 8: // Panel 8
-                character1.style.left = '10%';
-                character1.style.top = '820vh';
-                character2.style.left = '50%';
-                character2.style.top = '830vh';
+                character1.style.left = '10vw';
+                character1.style.top = '870vh';
+                character2.style.left = '100vw';
+                character2.style.top = '870vh';
                 break;
             case 9: // Panel 9
-                character1.style.left = '10%';
-                character1.style.top = '920vh';
-                character2.style.left = '50%';
-                character2.style.top = '930vh';
+                character1.style.left = '10vw';
+                character1.style.top = '970vh';
+                character2.style.left = '100vw';
+                character2.style.top = '970vh';
                 break;
             default:
-                character1.style.left = '10%';
-                character1.style.top = '80vh';
-                character2.style.left = '90%';
-                character2.style.top = '30vh';
+                character1.style.left = '10vw';
+                character1.style.top = '70vh';
+                character2.style.left = '100vw';
+                character2.style.top = '70vh';
         }
     }
 
+    function positionCharactersForDesktop(panelIndex) {
+        // Define positions for desktop screens
+        switch(panelIndex) {
+            case 0: // Header
+                character1.style.left = '10vw';
+                character1.style.top = '70vh';
+                character2.style.left = '100vw';
+                character2.style.top = '70vh';
+                console.log("0");
+                break;
+            case 1: // Panel 1
+                character1.style.left = '30vw';
+                character1.style.top = '170vh';
+                character2.style.left = '100vw';
+                character2.style.top = '170vh';
+                character3.style.left = '50vw';
+                character3.style.top = '150vh';
+                break;
+            case 2: // Panel 2
+                character1.style.left = '30vw';
+                character1.style.top = '270vh';
+                character2.style.left = '100vw';
+                character2.style.top = '270vh';
+                character3.style.left = '50vw';
+                character3.style.top = '250vh';
+                break;
+            case 3: // Panel 3
+                character1.style.left = '10vw';
+                character1.style.top = '370vh';
+                character2.style.left = '100vw';
+                character2.style.top = '370vh';
+                break;
+            case 4: // Panel 4
+                character1.style.left = '10vw';
+                character1.style.top = '470vh';
+                character2.style.left = '100vw';
+                character2.style.top = '470vh';
+                break;
+            case 5: // Panel 5
+                character1.style.left = '10vw';
+                character1.style.top = '570vh';
+                character2.style.left = '100vw';
+                character2.style.top = '570vh';
+                break;
+            case 6: // Panel 6
+                character1.style.left = '10vw';
+                character1.style.top = '670vh';
+                character2.style.left = '100vw';
+                character2.style.top = '670vh';
+                break;
+            case 7: // Panel 7
+                character1.style.left = '10vw';
+                character1.style.top = '770vh';
+                character2.style.left = '100vw';
+                character2.style.top = '770vh';
+                break;
+            case 8: // Panel 8
+                character1.style.left = '10vw';
+                character1.style.top = '870vh';
+                character2.style.left = '100vw';
+                character2.style.top = '870vh';
+                break;
+            case 9: // Panel 9
+                character1.style.left = '10vw';
+                character1.style.top = '970vh';
+                character2.style.left = '100vw';
+                character2.style.top = '970vh';
+                break;
+            default:
+                character1.style.left = '10vw';
+                character1.style.top = '70vh';
+                character2.style.left = '100vw';
+                character2.style.top = '70vh';
+        }
+    }
+
+    // Event listener for scroll
     window.addEventListener('scroll', function() {
         const panelIndexInView = getPanelIndexInView();
         moveCharacters(panelIndexInView);
     });
 
-    // Initial character setup based on the first element in view
+    // Event listener for window resize
+    window.addEventListener('resize', function() {
+        moveCharacters(getPanelIndexInView());
+    });
+
+    // Initial setup
     moveCharacters(getPanelIndexInView());
 });
--- scripts/thread.js
+++ scripts/thread.js
@@ -1,122 +1,165 @@
 window.onload = function() {
     const canvas = document.getElementById('redLineCanvas');
     const ctx = canvas.getContext('2d');
-    const character1 = document.getElementById('character1');
-    const character2 = document.getElementById('character2');
-    let highlight = document.getElementById('highlight'); // Initial highlight element
+    let character1 = document.getElementById('character1');
+    let character2 = document.getElementById('character2');
+    let highlight = document.querySelector('.highlight');
+    let currentPanelIndex = 0;
+
+    const panels = [document.querySelector('header'), ...document.querySelectorAll('#narrative > div'), document.querySelector('footer')];
+
+    function getPanelIndexInView() {
+        let closestIndex = -1;
+        let smallestDistance = Infinity;
+    
+        panels.forEach((panel, index) => {
+            const rect = panel.getBoundingClientRect();
+            const distanceToTop = Math.abs(rect.top);
+    
+            if (distanceToTop < smallestDistance) {
+                smallestDistance = distanceToTop;
+                closestIndex = index;
+            }
+        });
+    
+        return closestIndex;
+    }
 
     function updateCanvasSize() {
         canvas.width = window.innerWidth;
-        canvas.height = document.body.scrollHeight; // Height of the entire scrollable document
+        canvas.height = document.body.scrollHeight;
     }
 
     function drawWavyLine() {
+        if (!character1 || !character2 || !highlight) {
+            console.error("One or more required elements are missing.");
+            return;
+        }
+    
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+        
+        let opacity = 1; 
+        if (currentPanelIndex === 1) {
+            opacity = 0; 
+        } else {
+            const panelRect = panels[currentPanelIndex].getBoundingClientRect();
+            const distanceFromTop = Math.abs(panelRect.top);
+            const panelHeight = panelRect.height;
+            opacity = Math.max(0, 1 - (distanceFromTop / (panelHeight * 0.5)));
+        }
+    
+        if (opacity <= 0) {
+            return;
+        }
+    
+        ctx.strokeStyle = 'rgba(240, 91, 91,' + opacity + ')';
+        
         let startRect = character1.getBoundingClientRect();
         let endRect = character2.getBoundingClientRect();
-        let highlightRect = highlight.getBoundingClientRect(); // Updated to the current highlight element
-
+        let highlightRect = highlight.getBoundingClientRect();
+    
         let emSize = parseFloat(getComputedStyle(document.body).fontSize);
-
+    
         let startX = startRect.left + startRect.width / 2;
         let startY = startRect.top + startRect.height / 2 + window.scrollY;
         let endX = endRect.left + endRect.width / 2;
         let endY = endRect.top + endRect.height / 2 + window.scrollY;
-
-        let scribbleStartX = highlightRect.left + (emSize*4); // Adjusted position for scribble start
+    
+        let scribbleStartX = highlightRect.left + (emSize*4); 
         let scribbleStartY = highlightRect.top + window.scrollY + highlightRect.height + (emSize*6);
-
+    
         let amplitude = 20;
         let frequency = 1.5;
-
+    
         ctx.beginPath();
         ctx.moveTo(startX, startY);
-
-        // Draw wavy line to the scribble start point
+    
+        // Vary the width in the wavy line section
         for (let i = 0; i <= 1; i += 0.01) {
             let x = lerp(startX, scribbleStartX, i);
             let y = lerp(startY, scribbleStartY, i) + amplitude * Math.sin(frequency * i * 2 * Math.PI);
+            ctx.lineWidth = 2 + Math.sin(i * Math.PI) * 3; // Varies between 2 and 5
             ctx.lineTo(x, y);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(x, y);
         }
-
-        // Create curves that mimic random scribble loops
+    
         let lastX = scribbleStartX, lastY = scribbleStartY;
+        // Vary the width in the scribble loops section
         for (let i = 0; i < 15; i++) {
             let nextX = scribbleStartX + Math.random() * 100 - 50;
             let nextY = scribbleStartY + Math.random() * 100 - 50;
-
+    
             let cp1X = (lastX + nextX) / 2 + Math.random() * 50 - 25;
             let cp1Y = (lastY + nextY) / 2 + Math.random() * 50 - 25;
-
+    
             ctx.quadraticCurveTo(cp1X, cp1Y, nextX, nextY);
+            ctx.lineWidth = 2 + Math.random() * 3; // Randomly varies between 2 and 5
             lastX = nextX;
             lastY = nextY;
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(nextX, nextY);
         }
-
-        // Continue wavy line from scribble to end
+    
+        // Vary the width in the final wavy line section
         for (let i = 0; i <= 1; i += 0.01) {
             let x = lerp(lastX, endX, i);
             let y = lerp(lastY, endY, i) + amplitude * Math.sin(frequency * i * 2 * Math.PI);
+            ctx.lineWidth = 2 + Math.sin(i * Math.PI) * 3; // Varies between 2 and 5
             ctx.lineTo(x, y);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(x, y);
         }
-
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = '#f05b5b';
-        ctx.stroke();
     }
-
+    
     function lerp(start, end, t) {
         return start * (1 - t) + end * t;
     }
+    
 
     function updateHighlightElement() {
-        // Find the panel that occupies the most space in the viewport
-        const panels = [
-            document.querySelector('header'),
-            ...document.querySelectorAll('#narrative > div'),
-            document.querySelector('footer')
-        ];
-    
         let mostVisiblePanel = null;
         let maxVisibleArea = 0;
     
-        panels.forEach(panel => {
+        panels.forEach((panel, index) => {
             const rect = panel.getBoundingClientRect();
             const visibleArea = Math.max(0, Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0));
     
             if (visibleArea > maxVisibleArea) {
                 maxVisibleArea = visibleArea;
                 mostVisiblePanel = panel;
+                currentPanelIndex = index; 
             }
         });
     
         if (mostVisiblePanel) {
-            const newHighlight = mostVisiblePanel.querySelector('span#highlight');
-            if (newHighlight) {
-                highlight = newHighlight;
+            const newHighlightElements = mostVisiblePanel.getElementsByClassName('highlight');
+    
+            if (newHighlightElements.length > 0) {
+                highlight = newHighlightElements[0]; 
             }
         }
     }
-    
 
-    window.addEventListener('resize', function () {
+    window.addEventListener('resize', function() {
         updateCanvasSize();
         drawWavyLine();
     });
 
     window.addEventListener('scroll', function() {
-        // Update the highlight element first
+        currentPanelIndex = getPanelIndexInView(); 
         updateHighlightElement();
-    
-        // Use requestAnimationFrame to ensure drawing is synced with the browser's rendering
         requestAnimationFrame(function() {
             updateCanvasSize();
             drawWavyLine();
         });
     });
-    
-    // Initial canvas setup and draw
+
+    currentPanelIndex = getPanelIndexInView(); 
+    updateHighlightElement();
     updateCanvasSize();
     drawWavyLine();
 };
